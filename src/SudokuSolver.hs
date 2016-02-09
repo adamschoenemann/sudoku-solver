759f1cf9bb21b4c4bc448c8bf8271825bef53426
Adam Schønemann <adamschoenemann@gmail.com>
2016-02-09 18:22:08 +0100

    still kinda crappy
    Set feels that equality is also when ordering is the same, so it
    would nub elements that had the same utility
    
    Switched to SortedList, but seems a little slow

--- src/SudokuSolver.hs
+++ src/SudokuSolver.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
 module SudokuSolver where
 
@@ -16,7 +17,7 @@ import Data.Ord (comparing)
 import Control.Applicative
 import SudokuTypes
 import SudokuParser
-import qualified Data.Set as S
+import qualified Data.SortedList as S
 
 
 board :: Board
@@ -31,6 +32,55 @@ board =
             , [5, 0, 0, 0, 2, 4, 0, 0, 0]
             , [0, 4, 9, 0, 7, 0, 0, 0, 0]
             , [0, 0, 8, 0, 0, 3, 2, 4, 6]
+            ]
+        toCell x = if x == 0 then Nothing else Just x
+    in  V.fromList $ map (V.fromList . map toCell) board'
+
+board2 :: Board
+board2 =
+    let board' =
+            [ [6,9,3,7,2,5,8,1,4]
+            , [7,8,2,0,6,4,5,3,9]
+            , [1,0,5,8,9,3,7,2,6]
+            , [2,3,8,0,5,6,1,4,7]
+            , [5,7,9,4,1,2,6,8,3]
+            , [0,6,1,0,7,8,0,9,5]
+            , [8,1,7,5,3,9,4,6,2]
+            , [3,5,6,2,4,1,9,7,8]
+            , [9,2,4,6,8,7,3,5,1]
+            ]
+        toCell x = if x == 0 then Nothing else Just x
+    in  V.fromList $ map (V.fromList . map toCell) board'
+
+board3 :: Board
+board3 =
+    let board' =
+            [ [6,0,0,7,2,5,8,1,4]
+            , [7,0,2,0,6,4,5,3,9]
+            , [1,0,5,0,9,3,7,2,6]
+            , [2,3,0,0,5,6,1,4,7]
+            , [5,7,9,4,1,2,6,8,3]
+            , [0,6,1,0,0,8,0,9,5]
+            , [8,1,7,5,3,9,0,6,2]
+            , [3,5,6,2,4,1,0,7,8]
+            , [9,2,4,0,8,0,3,5,1]
+            ]
+        toCell x = if x == 0 then Nothing else Just x
+    in  V.fromList $ map (V.fromList . map toCell) board'
+
+
+board4 :: Board
+board4 =
+    let board' =
+            [ [0,0,3,0,2,0,6,0,0]
+            , [9,0,0,3,0,5,0,0,1]
+            , [0,0,1,8,0,6,4,0,0]
+            , [0,0,8,1,0,2,9,0,0]
+            , [7,0,0,0,0,0,0,0,8]
+            , [0,0,6,7,0,8,2,0,0]
+            , [0,0,2,6,0,9,5,0,0]
+            , [8,0,0,2,0,3,0,0,9]
+            , [0,0,5,0,1,0,3,0,0]
             ]
         toCell x = if x == 0 then Nothing else Just x
     in  V.fromList $ map (V.fromList . map toCell) board'
@@ -139,24 +189,60 @@ insertCell r c cell board =
                 board' = board // [(r, row')]
             in board'
 
-class (Show s, Eq s, Ord s, Ord a) => Searchable s a where
-    actions :: s -> S.Set a
+select :: Ord s => S.SortedList s -> Maybe (s, S.SortedList s)
+select states =
+    S.uncons states
+        -- else
+        --     let h = head $ S.fromSortedList states
+        --     in Just (h, states)
+
+
+class (Show s, Eq s, Ord s, Ord a) => Searchable s a | s -> a where
+    actions :: s -> S.SortedList a
     transition :: a -> s -> s
+    -- prune :: S.SortedList s -> S.SortedList s
 
-    search :: s -> (s -> Bool) -> Maybe [a]
-    search state goalTest = trace (show state) $
-        if goalTest state
-        then Just []
-        else
-            let acts = (actions state) :: S.Set a
-                trans a = (:) <$> Just a <*> search (transition a state) goalTest
-                substates = S.toAscList $ S.map trans acts
-            in if S.null acts
-                then Nothing
-                else join . find isJust $ substates
+    search' :: s -> S.SortedList s -> (s -> Bool) -> Maybe s
+    search' state frontier goalTest
+        | goalTest state = Just state
+        | otherwise =
+            let acts = (actions state) :: S.SortedList a
+                substates = S.map (flip transition $ state) acts
+                next = select $ (slUnion frontier substates)
+            in case next of
+                Nothing -> Nothing
+                Just (state', frontier') ->
+                    trace (show . slLength $ frontier')
+                        (search' state' frontier' goalTest)
+
+    -- search :: s -> S.SortedList s -> (s -> Bool) -> Maybe [a]
+    -- search state frontier goalTest
+    --     | goalTest state = Just []
+    --     | otherwise =
+    --         let acts = (actions state) :: S.SortedList a
+    --             substates = S.map (flip transition $ state) acts
+    --             next = select $ (slUnion frontier substates)
+    --         in case next of
+    --             Nothing -> Nothing
+    --             Just (state', frontier') -> (search state' frontier' goalTest)
+
+    -- search :: s -> (s -> Bool) -> Maybe [a]
+    -- search state goalTest =
+    --     if goalTest state
+    --     then Just []
+    --     else
+    --         let acts = (actions state) :: S.SortedList a
+    --             trans a = (:) <$> Just a <*> search (transition a state) goalTest
+    --             substates = S.toDescList $ S.map trans acts
+    --         in if S.null acts
+    --             then Nothing
+    --             else join . find isJust $ substates
 
 
-newtype SearchBoard = SearchBoard {unBoard :: Board} deriving Eq
+newtype SearchBoard = SearchBoard {unBoard :: Board}
+
+instance Eq SearchBoard where
+    (SearchBoard x) == (SearchBoard y) = x == y
 
 instance Show SearchBoard where
     show (SearchBoard b) = unlines $ showBoard b
@@ -166,19 +252,34 @@ utility (SearchBoard b) =
             V.sum . V.map length . join
             . mapWithIndices (thd . possibleCellValues' b) $ b
         empties = V.length $ getEmptyCells b
-    -- in (toRational empties) / (toRational possibilities)
-    in (toRational possibilities) / (toRational empties)
+    in empties-- ((toRational empties) / 9 * 9) / (toRational possibilities)
+    -- in if empties == 0
+    --    then 10000
+    --    else if possibilities == 0
+    --    then -10000
+    --    else if possibilities < empties
+    --    then -10000
+    --    else (toRational possibilities) / (toRational empties)
 
 instance Ord SearchBoard where
     compare x y = utility x `compare` utility y
         -- compare (utility x) (utility y)
 
 instance Searchable SearchBoard (Int, Int, Int) where
+
+    -- prune states =
+    --     S.filter f states where
+    --         f s = length $ getP
+
     actions (SearchBoard b) =
         let getActions (r, c, cell) =
                 let pos = possibleCellValues b (r,c,cell)
                 in  V.fromList $ map (\p -> (r,c,p)) pos
-        in S.fromList . V.toList . join . join . mapWithIndices getActions $ b
+            actions = join . join $ mapWithIndices getActions b
+        in if
+            V.length actions < (V.length $ getEmptyCells b)
+            then slEmpty
+            else S.toSortedList . V.toList $ actions
 
     transition (r, c, val) s =
         let b' = insertCell r c (Just val) (unBoard s)
@@ -186,6 +287,8 @@ instance Searchable SearchBoard (Int, Int, Int) where
             then SearchBoard b'
             else error "Should never happen!"
 
+isComplete :: Board -> Bool
+isComplete = V.null . getEmptyCells
 
 insertCells :: [(Int,Int,Int)] -> Board -> Board
 insertCells [] b = b
@@ -212,16 +315,15 @@ insertCells ((r,c,val):xs) b =
 -}
 -- solve :: Board -> Maybe Board
 solve board =
-    let result :: Maybe [(Int,Int,Int)]
-        test (SearchBoard b) = isValid b && (V.null . getEmptyCells $ b)
-        result = search (SearchBoard board) test
+    let test = isComplete . unBoard
+        result = search' (SearchBoard board) slEmpty test
     in result
 
-solveAndShow board =
-    let result = solve board
-    in case result of
-        Nothing   -> Nothing
-        Just acts -> Just $ insertCells acts board
+-- solveAndShow board =
+--     let result = solve board
+--     in case result of
+--         Nothing   -> Nothing
+--         Just acts -> Just $ insertCells acts board
 
 
 
@@ -251,10 +353,31 @@ solveAndShow board =
 
 thd (a,b,c) = c
 
--- solveFile file = do
---     contents <- readFile file
---     let sudokus = parseSudokus contents
---     case sudokus of
---         Left err -> putStrLn (show err)
---         Right sudokus' ->
---             maybe (putStrLn "unsolved") printBoard (solve $ sudokus' !! 1)
+solveFile file = do
+    contents <- readFile file
+    let sudokus = parseSudokus contents
+    case sudokus of
+        Left err -> putStrLn (show err)
+        Right sudokus' ->
+            maybe (putStrLn "unsolved") (printBoard . unBoard) (solve $ sudokus' !! 0)
+
+-- test set
+
+-- newtype SortedListTest = SortedListTest { unSortedListTest :: (String, Int) } deriving (Eq, Show)
+
+-- instance Ord SortedListTest where
+--     compare (SortedListTest (_,x)) (SortedListTest (_,y)) = compare x y
+
+slDelete :: (Ord a) => a -> S.SortedList a -> S.SortedList a
+slDelete x xs = S.filter (/= x) xs
+
+slUnion :: (Ord a) => S.SortedList a -> S.SortedList a -> S.SortedList a
+slUnion xs ys = union (S.fromSortedList xs) ys where
+    union (x:xs) l = union xs (x `S.insert` l)
+    union [] l = l
+
+slLength :: (Ord a) => S.SortedList a -> Int
+slLength = length . S.fromSortedList
+
+slEmpty :: (Ord a) => S.SortedList a
+slEmpty = S.toSortedList []
